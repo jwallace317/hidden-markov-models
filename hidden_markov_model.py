9bc048462b76a8019a716bec4f7bc486a704e60a
James  Wallace <jameswallace@Jamess-MBP.lan1>
2020-02-23 17:36:00 -0500

    started to implement viterbis algorithm

--- hidden_markov_model.py
+++ hidden_markov_model.py
@@ -5,8 +5,22 @@ import pandas as pd
 
 class HiddenMarkovModel():
 
-    def __init__(self, length):
-        self.length = length
+    def __init__(self, emission_probability, transition_probability):
+        self.emission_probability = emission_probability
+        self.transition_probability = transition_probability
 
-    def viterbi(self):
-        print('in viterbi method')
+    def viterbi(self, sequence):
+        previous = np.array([])
+        for i in range(self.emission_probability.shape[1]):
+            previous = np.append(
+                previous, self.transition_probability[i, 2] * self.emission_probability[sequence[0] - 1, i])
+
+        max_previous = np.max(previous)
+
+        max_state = []
+        max_probability = []
+        for i in np.arange(2, len(sequence) + 1):
+
+            current = np.array([])
+            for j in range(self.emission_probability.shape[1]):
+                current = np.append(current, self.transition_probability[])
--- main.py
+++ main.py
@@ -9,22 +9,30 @@ from hidden_markov_model import HiddenMarkovModel
 def main():
 
     # read in provided data
-    emission_probability = pd.read_csv('./lab3data/observationProbs.csv')
-    transition_probability = pd.read_csv('./lab3data/transitionProbs.csv')
-    data = pd.read_csv('./lab3data/testData.csv')
+    emission_probability = pd.read_csv(
+        './lab3data/observationProbs.csv').to_numpy()
+    transition_probability = pd.read_csv(
+        './lab3data/transitionProbs.csv').to_numpy()
+    data = pd.read_csv('./lab3data/testData.csv').to_numpy()
 
+    # remove the first column
+    emission_probability = emission_probability[:, 1:]
+    transition_probability = transition_probability[:, 1:]
+    data = data[:, 1:]
+
+    # print the head of the data frames
     print('emission probability')
-    print(emission_probability.head())
-
+    print(emission_probability)
     print('transition probability')
-    print(transition_probability.head())
-
+    print(transition_probability)
     print('test data')
-    print(data.head())
+    print(data)
 
-    length = 5
-    hmm = HiddenMarkovModel(length)
-    hmm.viterbi()
+    hmm = HiddenMarkovModel(emission_probability, transition_probability)
+
+    for sequence in range(data.shape[0]):
+        print(f'sequence { sequence } = { data[sequence, :] }')
+        hmm.viterbi(data[sequence, :])
 
 
 if __name__ == '__main__':
