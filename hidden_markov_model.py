ff90d393f25db051a2c072ccddfc034b3fcdaab0
James  Wallace <jameswallace@Jamess-MBP.lan1>
2020-02-23 23:31:04 -0500

    refactored hmm class to be more general use

--- hidden_markov_model.py
+++ hidden_markov_model.py
@@ -1,26 +1,41 @@
 # import necessary modules
 import numpy as np
-import pandas as pd
 
 
+# hidden markov model class
 class HiddenMarkovModel():
 
-    def __init__(self, emission_probability, transition_probability):
-        self.emission_probability = emission_probability
-        self.transition_probability = transition_probability
+    # initialize hidden markov model
+    def __init__(self, observed_states, unobserved_states, emissions, transitions, length):
+        self.observed_states = observed_states
+        self.unobserved_states = unobserved_states
+        self.emissions = emissions
+        self.transitions = transitions
+        self.length = length
+        self.t1 = np.zeros((len(unobserved_states) - 1, length))
+        self.t2 = np.zeros((len(unobserved_states) - 1, length))
 
     def viterbi(self, sequence):
-        previous = np.array([])
-        for i in range(self.emission_probability.shape[1]):
-            previous = np.append(
-                previous, self.transition_probability[i, 2] * self.emission_probability[sequence[0] - 1, i])
+        unobserved_dict = {}
+        for i, unobserved in enumerate(self.unobserved_states[1:]):
+            unobserved_dict[unobserved] = i
+            self.t1[i, 0] = self.transitions[i, len(
+                self.unobserved_states) - 1] * self.emissions[sequence[0] - 1, unobserved_dict[unobserved]]
+        print(unobserved_dict)
+        print(self.t1)
+        input()
 
-        max_previous = np.max(previous)
+        for i, observed in enumerate(sequence[1:], start=1):
+            print(f'i = { i }')
+            for j, unobserved in enumerate(self.unobserved_states[1:]):
+                print(f'observed = { observed } unobserved = { unobserved }')
+                print(self.t1[j, i - 1])
+                print(self.emissions[observed, unobserved_dict[unobserved]])
+                # self.t1[j, i] = self.t1[j, i - 1] *
+                # self.emissions[observed, unobserved_dict[unobserved]] *
+                # self.transitions[unobserved_dict[unobserved],
+                #                  unobserved_dict[prev_unobserved]]
+                # print(self.t1)
+                input()
 
-        max_state = []
-        max_probability = []
-        for i in np.arange(2, len(sequence) + 1):
-
-            current = np.array([])
-            for j in range(self.emission_probability.shape[1]):
-                current = np.append(current, self.transition_probability[])
+        return 1
--- main.py
+++ main.py
@@ -8,31 +8,48 @@ from hidden_markov_model import HiddenMarkovModel
 # task main
 def main():
 
-    # read in provided data
-    emission_probability = pd.read_csv(
-        './lab3data/observationProbs.csv').to_numpy()
-    transition_probability = pd.read_csv(
-        './lab3data/transitionProbs.csv').to_numpy()
-    data = pd.read_csv('./lab3data/testData.csv').to_numpy()
+    # read in provided csv data
+    emissions_df = pd.read_csv('./lab3data/observationProbs.csv')
+    transition_probability_df = pd.read_csv('./lab3data/transitionProbs.csv')
+    sequences_df = pd.read_csv('./lab3data/testData.csv')
 
-    # remove the first column
-    emission_probability = emission_probability[:, 1:]
-    transition_probability = transition_probability[:, 1:]
-    data = data[:, 1:]
-
-    # print the head of the data frames
+    # print the data frames
     print('emission probability')
-    print(emission_probability)
+    print(emissions_df.head())
     print('transition probability')
+    print(transition_probability_df.head())
+    print('test sequences')
+    print(sequences_df.head())
+
+    # get observed state space and the unobserved state space
+    observed_states = emissions_df['P(x|...)']
+    unobserved_states = list(emissions_df.columns)
+
+    # print the state spcaes
+    print(observed_states)
+    print(unobserved_states)
+
+    # convert the data frames to numpy arrays
+    emissions = emissions_df.to_numpy()
+    transition_probability = transition_probability_df.to_numpy()
+    sequences = sequences_df.to_numpy()
+
+    # print the trimmed emission and transition matrix
+    transition_probability = np.delete(transition_probability, 0, axis=1)
     print(transition_probability)
-    print('test data')
-    print(data)
+    emissions = np.delete(emissions, 0, axis=1)
+    print(emissions)
+    sequences = np.delete(sequences, 0, axis=1)
+    print(sequences)
 
-    hmm = HiddenMarkovModel(emission_probability, transition_probability)
+    # instantiate hidden markov model
+    hmm = HiddenMarkovModel(observed_states,
+                            unobserved_states,
+                            emissions,
+                            transition_probability,
+                            length=5)
 
-    for sequence in range(data.shape[0]):
-        print(f'sequence { sequence } = { data[sequence, :] }')
-        hmm.viterbi(data[sequence, :])
+    hmm.viterbi(sequences[0, :])
 
 
 if __name__ == '__main__':
