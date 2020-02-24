eddbd41ef90d4f5e48028afdcf8fcb7d4d447b12
James  Wallace <jameswallace@Jamess-MBP.lan1>
2020-02-24 16:09:00 -0500

    done working on and testing viterbi algorithm - time to work on pt. 2

--- hidden_markov_model.py
+++ hidden_markov_model.py
@@ -12,20 +12,20 @@ class HiddenMarkovModel():
         self.emissions = emissions  # emission matrix
         self.transitions = transitions  # transmission matrix
         self.length = length  # length of hidden markov model
-        self.t1 = np.zeros((len(state_space) - 1, length), dtype=np.float32)
-        self.t2 = np.zeros((len(state_space) - 1, length), dtype=np.int8)
+        self.t1 = np.zeros((len(state_space), length), dtype=np.float32)
+        self.t2 = np.zeros((len(state_space), length), dtype=np.int8)
 
     # viterbi algorithm to compute most likely state path given observations
     def viterbi(self, observations):
-        for i, state in enumerate(self.state_space[1:]):
+        for i in range(len(self.state_space)):
             self.t1[i, 0] = self.transitions[i, len(
-                self.state_space) - 1] * self.emissions[observations[0] - 1, i]
+                self.state_space)] * self.emissions[observations[0] - 1, i]
 
         for j, observation in enumerate(observations[1:], start=1):
             max_values = np.zeros(
-                (len(self.state_space) - 1, len(self.state_space) - 1))
-            for i, src_state in enumerate(self.state_space[1:]):
-                for k, dest_state in enumerate(self.state_space[1:]):
+                (len(self.state_space), len(self.state_space)))
+            for i in range(len(self.state_space)):
+                for k in range(len(self.state_space)):
                     max_values[i, k] = self.emissions[observation - 1,
                                                       i] * self.transitions[i, k] * self.t1[k, j - 1]
                 self.t1[i, j] = np.max(max_values[i, :])
@@ -37,21 +37,15 @@ class HiddenMarkovModel():
         print('\nself.t2')
         print(self.t2)
 
-        max_value = np.argmax(self.t1[:, self.length - 1])
-        # print(f'\nmax value = { max_value }')
-        max_state = self.state_space[max_value + 1]
-        # print(f'\nmax state = { max_state }')
+        max_index = np.argmax(self.t1[:, self.length - 1])
+        max_state = self.state_space[max_index]
 
-        max_values = [max_value]
         max_states = [max_state]
         for j, observation in zip(range(len(observations) - 1, 0, -1), observations[::-1]):
-            prev_max_state_index = self.t2[max_value, j]
-            print(f'\nprev max state index = { prev_max_state_index }')
-            prev_max_state = self.state_space[prev_max_state_index + 1]
-            # print(f'\nprev max state = { prev_max_state }')
+            prev_max_state_index = self.t2[max_index, j]
+            prev_max_state = self.state_space[prev_max_state_index]
             max_states.append(prev_max_state)
-            max_value = prev_max_state_index
-            # print(f'\nmax prob state path = { max_states }')
+            max_index = prev_max_state_index
 
         print('\nmax path')
         print(max_states[::-1])
--- main.py
+++ main.py
@@ -12,68 +12,73 @@ def main():
     # read in provided csv data
     emissions_df = pd.read_csv('./lab3data/observationProbs.csv')
     transitions_df = pd.read_csv('./lab3data/transitionProbs.csv')
-    sequences_df = pd.read_csv('./lab3data/testData.csv')
+    observations_df = pd.read_csv('./lab3data/testData.csv')
 
     # print the data frames
     print('\nemission probability data frame')
     print(emissions_df.head())
     print('\ntransition probability data frame')
     print(transitions_df.head())
-    print('\ntest sequences data frame')
-    print(sequences_df.head())
+    print('\nobservations data frame')
+    print(observations_df.head())
 
     # get observed space and state space
-    observations = emissions_df['P(x|...)']
-    states = list(emissions_df.columns)
+    observation_space = emissions_df['P(x|...)']
+    state_space = list(emissions_df.columns)[1:]
 
     # print the state spcaes
     print('\nobservation space')
-    print(observations)
+    print(observation_space)
     print('\nstate space')
-    print(states)
+    print(state_space)
 
     # convert the data frames to numpy arrays
     emissions = emissions_df.to_numpy()
     transitions = transitions_df.to_numpy()
-    sequences = sequences_df.to_numpy()
+    observations = observations_df.to_numpy()
 
     # trim the matrices of meta data
     emissions = np.delete(emissions, 0, axis=1)
     transitions = np.delete(transitions, 0, axis=1)
-    sequences = np.delete(sequences, 0, axis=1)
+    observations = np.delete(observations, 0, axis=1)
 
     # print the trimmed matrices
     print('\ntrimmed emission matrix')
     print(emissions)
     print('\ntrimmed transition matrix')
     print(transitions)
-    print('\ntrimmed sequence vector')
-    print(sequences)
+    print('\ntrimmed observations matrix')
+    print(observations)
 
     max_state_paths = []
-    for sequence in sequences:
-        sequence = sequence[sequence != 0]
-        print(f'\nsequence = { sequence }')
+    for observation in observations:
+        # trim observation
+        observation = observation[observation != 0]
+
+        # print trimmed observation
+        print(f'\nobservation = { observation }')
 
         # instantiate hidden markov model
-        hmm = HiddenMarkovModel(observations,
-                                states,
+        hmm = HiddenMarkovModel(observation_space,
+                                state_space,
                                 emissions,
                                 transitions,
-                                length=len(sequence))
+                                length=len(observation))
 
         # viterbi algorithm
-        max_prob_state_path = hmm.viterbi(sequence)
+        max_state_path = hmm.viterbi(observation)
 
-        max_state_paths.append(max_prob_state_path)
+        # append the max state path to list of max state paths
+        max_state_paths.append(max_state_path)
 
+    # print the max state paths given the observed sequences
     print('\n-------------------------MAX STATE PATHS GIVEN OBSERVED SEQUENCE-------------------------')
     print('{:^30s} {:^30s} {:^30s}'.format(
         'index', 'observed sequence', 'max state path'))
-    for i, sequence in enumerate(sequences):
-        sequence = sequence[sequence != 0]
+    for i, observation in enumerate(observations):
+        observation = observation[observation != 0]
         print('{:^30d} {:^30s} {:^30s}'.format(
-            i, str(sequence), str(max_state_paths[i])))
+            i, str(observation), str(max_state_paths[i])))
 
 
 if __name__ == '__main__':
